3f7604da0f4f743f93c68e3f5f4ee8d4754fac83
Tatjana <g00425678@atu.ie>
2023-11-03 13:20:30 +0100

    started assignment04

---
+++ Assignments/config.py
@@ -0,0 +1,8 @@
+# Reference: 
+# https://github.com/andrewbeattycourseware/datarepresentation/blob/main/code/Topic05-authentication/labs/config.py
+
+config = {
+    "htmltopdfkey":"eAs3mWn7R59gxZ6kSYysYdpTUsivY7Am74aSN36pOHdMMyTu3XDpdSOtaAySz3qF",
+    "githubkey" : "github_pat_replace with yours"
+}
+# https://github.com/Tanjasta/aprivateone.git
